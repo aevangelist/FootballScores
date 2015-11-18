f3a7bb6f6fb72feb7bf38b8b0ca207d8450ca611
Aleli Evangelista <hello@alelievangelista.com>
2015-11-18 17:18:59 -0500

    Initial commit

---
+++ app/src/main/java/barqsoft/footballscores/PagerFragment.java
@@ -0,0 +1,131 @@
+package barqsoft.footballscores;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.text.format.Time;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by yehya khaled on 2/27/2015.
+ */
+public class PagerFragment extends Fragment
+{
+    public static final int NUM_PAGES = 5;
+    public ViewPager viewPager;
+    private PageAdapter pageAdapter;
+    private MainScreenFragment[] viewFragments = new MainScreenFragment[5];
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
+    {
+        View rootView = inflater.inflate(R.layout.pager_fragment, container, false);
+
+        viewPager = (ViewPager) rootView.findViewById(R.id.pager);
+        TabLayout tabLayout = (TabLayout) rootView.findViewById(R.id.tab_layout);
+        pageAdapter = new PageAdapter(getChildFragmentManager());
+
+        for (int i = 0;i < NUM_PAGES;i++)
+        {
+            Date fragmentdate = new Date(System.currentTimeMillis()+((i-2)*86400000));
+            SimpleDateFormat mformat = new SimpleDateFormat("yyyy-MM-dd");
+            viewFragments[i] = new MainScreenFragment();
+            viewFragments[i].setFragmentDate(mformat.format(fragmentdate));
+
+            //Set the tab labels
+            tabLayout.addTab(tabLayout.newTab().setText(pageAdapter.getPageTitle(i).toString().toUpperCase()));
+        }
+
+        viewPager.setAdapter(pageAdapter);
+        viewPager.setCurrentItem(MainActivity.current_fragment);
+
+        //Add page listener
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.setOnTabSelectedListener(onTabSelectedListener(viewPager));
+
+        return rootView;
+    }
+
+    private TabLayout.OnTabSelectedListener onTabSelectedListener(final ViewPager pager) {
+        return new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                pager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        };
+    }
+
+    private class PageAdapter extends FragmentStatePagerAdapter
+    {
+        @Override
+        public Fragment getItem(int i)
+        {
+            return viewFragments[i];
+        }
+
+        @Override
+        public int getCount()
+        {
+            return NUM_PAGES;
+        }
+
+        public PageAdapter(FragmentManager fm)
+        {
+            super(fm);
+        }
+        // Returns the page title for the top indicator
+        @Override
+        public CharSequence getPageTitle(int position)
+        {
+            return getDayName(getActivity(),System.currentTimeMillis()+((position-2)*86400000));
+        }
+        public String getDayName(Context context, long dateInMillis) {
+            // If the date is today, return the localized version of "Today" instead of the actual
+            // day name.
+
+            Time t = new Time();
+            t.setToNow();
+            int julianDay = Time.getJulianDay(dateInMillis, t.gmtoff);
+            int currentJulianDay = Time.getJulianDay(System.currentTimeMillis(), t.gmtoff);
+            if (julianDay == currentJulianDay) {
+                return context.getString(R.string.today);
+            } else if ( julianDay == currentJulianDay +1 ) {
+                return context.getString(R.string.tomorrow);
+            }
+             else if ( julianDay == currentJulianDay -1)
+            {
+                return context.getString(R.string.yesterday);
+            }
+            else
+            {
+                Time time = new Time();
+                time.setToNow();
+                // Otherwise, the format is just the day of the week (e.g "Wednesday".
+                SimpleDateFormat dayFormat = new SimpleDateFormat("EEEE");
+                return dayFormat.format(dateInMillis);
+            }
+        }
+
+    }
+}
