ef4fd96f800bd222b9e072873a93164eeaab1551
aevangelist <evangelista.aleli@gmail.com>
2015-12-03 11:49:11 -0500

    Added "Refresh" action on widget toolbar

--- app/src/main/java/barqsoft/footballscores/widget/WidgetProvider.java
+++ app/src/main/java/barqsoft/footballscores/widget/WidgetProvider.java
@@ -41,7 +41,7 @@ public class WidgetProvider extends AppWidgetProvider {
     private RemoteViews updateWidgetListView(Context context,
                                              int appWidgetId) {
 
-        //which layout to show on widget
+        //Layout for widget
         RemoteViews remoteViews = new RemoteViews(
                 context.getPackageName(),R.layout.widget);
 
@@ -49,8 +49,6 @@ public class WidgetProvider extends AppWidgetProvider {
         Intent svcIntent = new Intent(context, WidgetService.class);
         //passing app widget id to that RemoteViews Service
         svcIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
-        //setting a unique Uri to the intent
-        //don't know its purpose to me right now
         svcIntent.setData(Uri.parse(
                 svcIntent.toUri(Intent.URI_INTENT_SCHEME)));
 
